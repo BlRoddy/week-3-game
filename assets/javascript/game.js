724fe619687c9f70dcb74d32077cd92d55bc359e
Blake Roddy <you@example.com>
2016-08-17 18:11:48 -0500

    html, js update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,34 @@
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-				'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+//list of words
+var wordList = ['ocarina', 'harpsichord', 'banjo', 'digeridoo', 'theramin'];	
 
-var words = ['ocarina', 'harpsichord', 'banjo', 'digeridoo', 'theramin']				
+// randomly selects word from list
+var word = wordList[Math.floor(Math.random()*wordList.length)];	
+console.log(word);
+
+var guesses = 15;
+
+var wins = 0;
+
+
+
+//catches keystroke of user
+document.onkeyup = function(event){
+
+	//keystroke to lower case
+	var letter = String.fromCharCode(event.keyCode).toLowerCase();
+	console.log(letter);
+
+	//reduces guesses by 1 on each keystroke
+	if(letter){
+		guesses--;
+	}
+	console.log(guesses);
+
+	//resets the page after all guesses are lost
+	if(guesses === 0){
+
+		alert("You have no guesses left! Try Again!");
+		location.reload();
+	}
+
+}
