e3b94b9a47ea536ad19e905b4a1231a3821c9ffe
Blake Roddy <you@example.com>
2016-08-19 17:31:57 -0500

    updated java, some html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,58 @@
-//list of words
+
 var wordList = ['ocarina', 'harpsichord', 'banjo', 'digeridoo', 'theramin'];	
-
-// randomly selects word from list
 var word = wordList[Math.floor(Math.random()*wordList.length)];	
-console.log(word);
-
-var guesses = 15;
-
+var blank = [];
+var guesses = 10;
 var wins = 0;
+var guessedLetters = [];
+var wordLetters = [];
 
+wordLetters.push(word);
 
+for (var i = 0; i < word.length; i++) {
+blank.push("_")
+}
+
+console.log(word);
+console.log(blank);
+console.log(wordLetters);
 
 //catches keystroke of user
 document.onkeyup = function(event){
+
 
 	//keystroke to lower case
 	var letter = String.fromCharCode(event.keyCode).toLowerCase();
 	console.log(letter);
 
-	//reduces guesses by 1 on each keystroke
-	if(letter){
-		guesses--;
+	//pushes letters used into guessed Letters
+	guessedLetters.push(letter);
+
+
+	//pushes correct letter into corresponding blank space
+	for (var i = 0; i < word.length; i++) {
+
+			if(letter === word.charAt(i)){
+				blank[i] = letter;
+				console.log(blank);
+				var check = true;
+			}
 	}
-	console.log(guesses);
+	
+	//if else statement checking for key match
+	if (check === true){
+
+			console.log(guesses);
+			console.log(guessedLetters);
+	}
+	//reduces guesses left if incorrect letter guessed
+	else{
+			guesses--;
+			console.log(guesses);
+			console.log(guessedLetters);
+	}
+
+
 
 	//resets the page after all guesses are lost
 	if(guesses === 0){
